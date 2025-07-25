4c62ee678ca63782a691381fe95fe1663a1f90ad
Dinesh Kumar <159294017+DineshKumar930@users.noreply.github.com>
2025-07-25 07:55:39 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
 import "./index.css";
 import App from "./App.jsx";
-import MobileApp from "./Assignment/MobileApp.jsx";
+import StopWatchApp from "./Assignment/StopWatchApp.jsx";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     
-    <MobileApp/>
+    <StopWatchApp/>
   </StrictMode>,
 )
